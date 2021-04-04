882d974f0a6750149be57205dd6b0cabdfd237d3
ighilarion <ighilarion.1990@gmail.com>
2021-04-04 13:16:54 +0200

    ss

--- src/views/FormPreparador/Presentacion.js
+++ src/views/FormPreparador/Presentacion.js
@@ -8,18 +8,24 @@ import * as yup from "yup";
 import { Button } from 'primereact/button';
 import { AutoComplete } from 'primereact/autocomplete';
 import sectorJSON from '../../assets/json/sectores.json';
+import perfilJSON from '../../assets/json/perfiles.json';
+
 import idiomasJSON from '../../assets/json/idiomas.json';
 import { InputText } from "primereact/inputtext";
+import { Form, Input, Upload, Space, Select, TimePicker, Row, Col, Tag, InputNumber } from 'antd';
+import ImgCrop from 'antd-img-crop';
+import TextArea from "antd/lib/input/TextArea";
+import { Option } from "antd/lib/mentions";
 
 const Presentacion = (props) => {
     const [sectores, setSectores] = useState([]);
     const [idiomas, setIdiomas] = useState(null)
     const [nombreImagen, setNombreImagen] = useState("")
-    const [nombrePerfil, setNombrePerfil]=useState(props.valores.nombrePerfil)
-    const [imgPefil, setImgPerfil]=useState(props.valores.imgPefil!==""?props.valores.imgPefil:null)
-    const [selectedSectores, setSelectedSectores] = useState(props.valores.sectores!==""?props.valores.sectores:null);
+    const [nombrePerfil, setNombrePerfil] = useState(props.valores.nombrePerfil)
+    const [imgPefil, setImgPerfil] = useState(props.valores.imgPefil !== "" ? props.valores.imgPefil : null)
+    const [selectedSectores, setSelectedSectores] = useState(props.valores.sectores !== "" ? props.valores.sectores : null);
     const [filteredSectores, setFilteredSectores] = useState(null);
-    const [selectedIdiomas, setSelectedIdiomas] = useState(props.valores.idiomas!==""?props.valores.idiomas:null);
+    const [selectedIdiomas, setSelectedIdiomas] = useState(props.valores.idiomas !== "" ? props.valores.idiomas : null);
     const [filteredIdiomas, setFilteredIdiomas] = useState(null);
 
     useEffect(() => {
@@ -79,7 +85,7 @@ const Presentacion = (props) => {
             bandera = false
         }
         if (bandera) {
-            values.nombrePerfil=nombrePerfil
+            values.nombrePerfil = nombrePerfil
             values.imagenperfil = imgPefil
             values.sectores = selectedSectores
             values.idiomas = selectedIdiomas
@@ -108,7 +114,7 @@ const Presentacion = (props) => {
         })
         return respuesta
     }
-    const onChangeImg = (e) => { 
+    const onChangeImg = (e) => {
         let file = e.target.files[0]
         if (file) {
             const reader = new FileReader()
@@ -137,112 +143,174 @@ const Presentacion = (props) => {
         };
         fileReader.readAsDataURL(file);*/
     };
+
+    const onPreview = async file => {
+        let src = file.url;
+        if (!src) {
+            src = await new Promise(resolve => {
+                const reader = new FileReader();
+                reader.readAsDataURL(file.originFileObj);
+                reader.onload = () => resolve(reader.result);
+            });
+        }
+        const image = new Image();
+        image.src = src;
+        const imgWindow = window.open(src);
+        imgWindow.document.write(image.outerHTML);
+    };
+
+    const onChange = ({ fileList: newFileList }) => {
+        setFileList(newFileList);
+        setImgPerfil(newFileList);
+
+    };
+
+    const [fileList, setFileList] = useState([] /*[
+        {
+          uid: '-1',
+          name: 'image.png',
+          status: 'done',
+          url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+        },
+      ]*/);
+
+    const sectoresOptions = [];
+    sectorJSON.sectores.map((sector) => {
+        sectoresOptions.push(<Option key={sector.name}>{sector.name}</Option>);
+    });
+
+    const perfilesOptions = [];
+    perfilJSON.perfiles.map((perfil) => {
+        perfilesOptions.push(<Option key={perfil.name}>{perfil.name}</Option>);
+    });
+
+    const idiomasOptions = [];
+    idiomasJSON.idiomas.map((idioma) => {
+        idiomasOptions.push(<Option key={idioma.nombre}>{idioma.nombre}</Option>);
+    });
+
     return (
 
-        <GridContainer>
-            <GridItem xs={12} sm={12} md={12}>
-                <div xs={12} sm={12} md={10} className={"left40px margintop50px"}>
-                    <Formik initialValues={valoresIniciales} onSubmit={handleSubmit} validationSchema={validationSchema}>
-                        {({ values, errors, handleSubmit }) => (
-                            <form onSubmit={handleSubmit} className="register-form">
-                                <GridContainer >
-                                    <GridItem xs={12} sm={12} md={5}>
-                                        <div className=" p-col-12">
-                                            <div>
-                                                <span className="p-float-label">
-                                                    <InputText id="in" value={nombrePerfil} onChange={(e)=>{setNombrePerfil(e.target.value)}} name="nombrePerfil" className="p-invalid" />
-                                                    <label htmlFor="in">Nombre de tu perfil</label>
-                                                </span>
-                                            </div>
-                                            <div><ErrorMessage name={"nombrePerfil"} className="invalid-feedback">{message => <div><small className="p-error p-d-block">{message}</small></div>}</ErrorMessage></div>
-                                        </div>
-                                        <div className=" p-col-12">
-                                            <label htmlFor={"annosExperiencia"} className="text textMarca">Años de experiencia en RRHH</label>
-                                            <div>
-                                                <Field id="annosExperiencia" name="annosExperiencia" type="number" className="with100" />
-                                            </div>
-                                            <div><ErrorMessage name={"annosExperiencia"} className="invalid-feedback">{message => <div><small className="p-error">{message}</small></div>}</ErrorMessage></div>
-                                        </div>
-                                        <div className="p-col-12">
-                                            <label htmlFor={"experiencia"} className="text textMarca">Tu experiencia (máx. 40 caracteres)</label>
-                                            <div>
-                                                <Field type="textarea" id="experiencia" name="experiencia" aria-describedby="username2-help" className="with100 height80px p-invalid p-d-block" />
-                                            </div>
-                                            <div><ErrorMessage name={"experiencia"} className="invalid-feedback">{message => <div><small className="p-error">{message}</small></div>}</ErrorMessage></div>
-                                        </div>
-                                    </GridItem>
 
-                                    <GridItem xs={12} sm={12} md={5}>
-                                        <GridContainer>
-                                            <GridItem xs={12} sm={12} md={12}>
-                                                <div className=" p-col-12">
-                                                    <label htmlFor={"imagenperfil"} className="text textMarca">Imagen de perfil</label>
-                                                    <div>
-                                                        <Field id="imagenperfilt"  name="imagenperfilt" onChange={(e) => { onChangeImg(e) }} type="file" className="with100" />
-                                                    </div>
-                                                    <div><ErrorMessage name={"imagenperfil"} className="invalid-feedback">{message => <div><small className="p-error">{message}</small></div>}</ErrorMessage></div>
-                                                </div>
-                                            </GridItem>
-                                            <GridItem xs={12} sm={12} md={12}>
-                                                <div className=" p-col-12">
-                                                    <label htmlFor={"sectores"} className="text textMarca">Sectores</label>
-                                                    <span className="p-fluid">
-                                                        <AutoComplete value={selectedSectores} suggestions={filteredSectores} completeMethod={searchSector} field="name" multiple onChange={(e) => setSelectedSectores(e.value)} />
-                                                    </span>
 
-                                                    <div><ErrorMessage name={"sectores"} className="invalid-feedback">{message => <div><small className="p-error">{message}</small></div>}</ErrorMessage></div>
-                                                </div>
-                                            </GridItem>
-                                            <GridItem xs={12} sm={12} md={12}>
-                                                <div className="p-col-12">
-                                                    <label htmlFor={"perfiles"} className="text textMarca">Perfiles</label>
-                                                    <div>
-                                                        <Field id="perfiles" name="perfiles" type="text" className="with100" />
-                                                    </div>
-                                                    <div><ErrorMessage name={"perfiles"} className="invalid-feedback">{message => <div><small className="p-error">{message}</small></div>}</ErrorMessage></div>
-                                                </div>
-                                            </GridItem>
-                                            <GridItem xs={12} sm={12} md={12}>
-                                                <div className="p-col-12">
-                                                    <label htmlFor={"idiomas"} className="text textMarca">Idiomas</label>
-                                                    <span className="p-fluid">
-                                                        <AutoComplete value={selectedIdiomas} suggestions={filteredIdiomas} completeMethod={searchIdioma} field="nombre" multiple onChange={(e) => setSelectedIdiomas(e.value)} />
-                                                    </span>
-                                                    <div><ErrorMessage name={"idiomas"} className="invalid-feedback">{message => <div><small className="p-error">{message}</small></div>}</ErrorMessage></div>
-                                                </div>
-                                            </GridItem>
-                                        </GridContainer>
-                                    </GridItem>
-                                </GridContainer>
-                                <GridContainer>
-                                    <GridItem xs={12} sm={12} md={2}>
-                                        <div className="p-field p-col p-md-6 p-col-12" >
-                                            <div className={"center"} >
-                                                <Button label="Anterior" type="button" onClick={()=>{props.goToStep(1)}}icon="pi pi-times" />
-                                            </div>
-                                        </div>
-                                    </GridItem>
-                                    <GridItem xs={12} sm={12} md={2}>
-                                        <div className="p-field p-col p-md-6 p-col-12" >
-                                            <div className={"center"} >
-                                                <Button label="Siguiente" type="submit" icon="pi pi-check" />
-                                            </div>
-                                        </div>
-                                    </GridItem>
-                                </GridContainer>
-                            </form>
-                        )}
-                    </Formik>
-                </div>
-            </GridItem>
-        </GridContainer>
+        <Form layout="vertical" className="register-form" name="formPresent" onFinish={handleSubmit} autoComplete="off">
+            <Row className="OfertaFrom">
+                <Col span={12} className="OfertaCol1">
+                    <Form.Item
+                        label="Nombre de tu perfil"
+                        name="nombrePerfil"
+
+                        rules={[{
+                            required: nombrePerfil === "" ? true : false,
+                            message: 'Por favor introduzca el nombre de su perfil',
+                        },]}
+                    >
+                        <Input
+                            defaultValue={nombrePerfil}
+                            id="in"
+                            value={nombrePerfil}
+                            placeholder="Nombre de tu perfil"
+                            onChange={(e) => { setNombrePerfil(e.target.value) }}
+                            name="nombrePerfil"
+                        />
+                    </Form.Item>
+
+                    <Form.Item
+                        label="Años de experiencia en RRHH"
+                        name="annosExperiencia"
+                        rules={[{
+                            required: true,
+                            message: 'Por favor introduzca los años de experiencia',
+                        },]}
+                    >
+                        <InputNumber name="annosExperiencia" min={0} max={70} placeholder={3} />
+                    </Form.Item>
+
+                    <Form.Item
+                        label="Tu experiencia (máx. 250 caracteres)"
+                        name="experiencia"
+                        rules={[{
+                            required: true,
+                            message: 'Por favor introduzca los años de experiencia',
+                        },]}
+                    >
+                        <TextArea showCount rows={6} maxlength={250} maxLength={250} id="experiencia" name="experiencia" aria-describedby="username2-help" />
+                    </Form.Item>
+
+                </Col>
+
+                <Col span={12} className="OfertaCol1">
+
+                    <Form.Item
+                        label="Imagen de perfil"
+                        name="imagenperfilt"
+                    >
+                        <ImgCrop rotate>
+                            <Upload
+                                fileList={fileList}
+                                onChange={onChange}
+                                onPreview={onPreview}
+                                listType="picture-card"
+                                name="imagenperfilt"
+                                id="imagenperfilt"
+
+                            >
+                                {fileList.length < 1 && '+ Adiciona tu imagen de perfil'}
+                            </Upload>
+                        </ImgCrop>
+                    </Form.Item>
+
+                    <Form.Item
+                        label="Sectores"
+                        name="sectores"
+                    >
+                        <Select mode="tags" value={selectedSectores} style={{ width: '100%' }} placeholder="Sectores" onChange={(e) => setSelectedSectores(e.value)}>
+                            {sectoresOptions}
+                        </Select>
+                    </Form.Item>
+
+                    <Form.Item
+                        label="Perfiles"
+                        name="perfiles"
+                    >
+                        <Select id="perfiles" mode="tags" style={{ width: '100%' }} placeholder="Perfiles" >
+                            {perfilesOptions}
+                        </Select>
+                    </Form.Item>
+
+                    <Form.Item
+                        label="Idiomas"
+                        name="idiomas"
+                    >
+                        <Select mode="tags" value={selectedIdiomas} style={{ width: '100%' }} placeholder="Idiomas" onChange={(e) => setSelectedIdiomas(e.value)}>
+                            {idiomasOptions}
+                        </Select>
+                    </Form.Item>
+
+                </Col>
+
+            </Row>
+
+            <GridContainer>
+                <GridItem xs={12} sm={12} md={2}>
+                    <div className="p-field p-col p-md-6 p-col-12" >
+                        <div className={"center"} >
+                            <Button label="Anterior" type="button" onClick={() => { props.goToStep(1) }} icon="pi pi-times" />
+                        </div>
+                    </div>
+                </GridItem>
+                <GridItem xs={12} sm={12} md={2}>
+                    <div className="p-field p-col p-md-6 p-col-12" >
+                        <div className={"center"} >
+                            <Button label="Siguiente" type="submit" icon="pi pi-check" />
+                        </div>
+                    </div>
+                </GridItem>
+            </GridContainer>
+        </Form >
+
+
     )
 };
 export default Presentacion;
 
-const validationSchema = yup.object().shape({
-    // nombrePerfil: yup.string().required("Nombre de perfil requerido."),
-    //annosExperiencia: yup.string().required("Años de experiencia requerido."),
-    //experiencia: yup.string().required("Experiencia requerido."),
-    //perfiles: yup.string().required("Perfiles requerido.")
-});
