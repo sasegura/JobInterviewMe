c3f55e9a2a1333fcc390a73c359366b249e58359
ighilarion <ighilarion.1990@gmail.com>
2021-04-11 00:56:38 +0200

    sssss

--- src/components/Loading/Loading.js
+++
@@ -1,8 +0,0 @@
-import React  from 'react';
-import loading from "assets/img/Loading.gif"
-const Loading=()=>{
-    return(<div className="center">
-                <img src={loading}/>
-            </div>)
-}
-export default Loading;
